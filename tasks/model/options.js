1a9e241f9580f08fe1f8f436e20f71524ec3392e
NHudkov <godba11@yandex.ru>
2017-10-09 13:29:31 +0500

    update .gitignore

--- tasks/model/options.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Options {
-}
-exports.Options = Options;
-class DbOptions {
-}
-exports.DbOptions = DbOptions;
-//# sourceMappingURL=options.js.map
--- tasks/model/schema.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Schema {
-}
-exports.Schema = Schema;
-//# sourceMappingURL=schema.js.map
