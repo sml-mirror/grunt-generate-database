87817fa1c131199670c6c2c6d360176c05199a96
MGorshkov <mgorshkov@softmedialab.com>
2017-12-26 14:17:15 +0500

    npm version update

--- test/src/model/hero/hero.ts
+++ test/src/model/hero/hero.ts
@@ -10,6 +10,6 @@ export class Hero {
     public data: string;
     @Column()
     public detailId?: number;
-    @Column("int", { "isArray": true })
+    @Column({"type": "integer", "array": true})
     public simpleArray: number[];
 }
