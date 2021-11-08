02d3202bca6f35ffc61a7480ebeff43b44815e27
khudkov <93304598+khudkov@users.noreply.github.com>
2021-11-08 16:45:01 +0500

    #20

--- src/tasks/shared/index.ts
+++ src/tasks/shared/index.ts
@@ -1,8 +1,9 @@
 export const emptyString = "";
-export const triggerEnabledDbTypes: string[] = ["postgres"];
+export const triggerEnabledDbTypes: string[] = ["postgres", "mssql"];
 
 export const triggerFunctionTemplateFileName = "createTriggerFunctionTemplate.njk";
 export const triggerTemplateFileName = "createTriggerTemplate.njk";
+export const triggerMssqlFileName = "createTrigger.njk";
 export const dbWrapperTemplateFileName = "createDBWrapper.njk";
 export const utf8String = "utf-8";
 export const pathToDeclaration = "./declaration.json";
