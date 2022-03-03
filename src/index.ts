391dffb4879311df482af1f09592ebc3c91a9682
khudkov <93304598+khudkov@users.noreply.github.com>
2022-03-03 16:15:25 +0500

    #25

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 import {CreateDbSCriptsInternal} from "./tasks/createDbScripts";
-import {Options} from "./tasks/model/options";
 
 export function CreateDbSCripts(): void {
     CreateDbSCriptsInternal();
-
 }
-
-
-
